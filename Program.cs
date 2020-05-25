74fa23284569fbba9af71368a851f0210728f280
Colby Whidden <51108569+ColbyWhidden@users.noreply.github.com>
2020-05-25 06:43:52 -0500

    added age var and sentence structure

--- Program.cs
+++ Program.cs
@@ -14,25 +14,24 @@ namespace BMI
             int tWeight = 50;
             float tHeight = 1.565F;
             float tBmi = tWeight / tHeight;
+            int tAge = 19;
 
             int cWeight = 68;
             float cHeight = 1.55F;
             float cBmi = cWeight / cHeight;
+            int cAge = 20;
 
             int bWeight = 61;
             float bHeight = 1.41F;
             float bBmi = bWeight / bHeight;
+            int bAge = 32;
 
             Console.Write("Hello world!" + System.Environment.NewLine);
 
-            // System.Environment.NewLine lets us send the next text lines to a new line ... duh
-
-            Console.WriteLine("Tom's height is " + tHeight + " and his weight is " + tWeight + " BMI is: " + tBmi);
-            Console.WriteLine("Colby's height is " + cHeight + " and his weight is " + cWeight + " BMI is: " + cBmi);
-            Console.WriteLine("Blakes's height is " + bHeight + " and his weight is " + bWeight + " BMI is: " + bBmi);
+            Console.WriteLine("Tom's age is " + tAge + "and his height is " + tHeight + " and his weight is " + tWeight + " BMI is: " + tBmi);
+            Console.WriteLine("Colby's age is " + cAge + "and his height is " + cHeight + " and his weight is " + cWeight + " BMI is: " + cBmi);
+            Console.WriteLine("Blakes's age is " + bAge + " and his height is " + bHeight + " and his weight is " + bWeight + " BMI is: " + bBmi);
             Console.ReadLine();
-
-
         }
     }
 }
