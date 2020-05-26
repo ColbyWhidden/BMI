4d061c19e24b18d4e50624c2759810b4e733b8c6
Colby Whidden <51108569+ColbyWhidden@users.noreply.github.com>
2020-05-26 17:10:07 -0500

    reworked bmi calculation, now only take non metric units

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Threading;
 
 namespace BMI
 {
@@ -18,8 +16,8 @@ namespace BMI
 
             Console.Write("Hello world!" + System.Environment.NewLine);
 
-            Console.WriteLine( tFirst + (" ")+ tSur + "'s age is " + tAge + " and his height is " + tHeight + " and his weight is " + tWeight + " BMI is: " + tBmi);
-            
+            Console.WriteLine(tFirst + (" ") + tSur + "'s age is " + tAge + " and his height is " + tHeight + " and his weight is " + tWeight + " BMI is: " + tBmi);
+
 
             Console.WriteLine("Press any key to continue...");
 
@@ -28,7 +26,6 @@ namespace BMI
 
             for (int i = 0; i < 2; i++)
             {
-
 
                 Console.WriteLine("Please enter your first name. ");
                 string firstName = Console.ReadLine();
@@ -41,14 +38,15 @@ namespace BMI
                 Console.WriteLine("Please enter your height now. ");
                 double yourHeight = double.Parse(Console.ReadLine());
                 Console.WriteLine("Please enter your weight now. ");
-                int yourWeight = Convert.ToInt32(Console.ReadLine());
-                double yourBMI = yourWeight / yourHeight;
-                Console.WriteLine("Alright " + firstName + ", " + "you are " + yourAge + " years old, and your BMI based off your provided measurements is " + yourBMI);
+                double yourWeight = double.Parse(Console.ReadLine());
+                double yourBMI = yourWeight / (yourHeight * yourHeight);
+                Console.WriteLine("Alright " + firstName + " " + surname + ", " + "you are " + yourAge + " years old, and your BMI based off your provided measurements is " + yourBMI);
 
                 Console.WriteLine("Press enter continue...");
 
                 Console.ReadLine();
                 Console.Clear();
+
             }
 
 
